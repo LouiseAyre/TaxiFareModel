44260fe62ca2e993a19116f7981007948fdebcdf
louise ayre <louise_ayre@hotmail.com>
2022-07-21 20:18:21 +1000

    added everything

---
+++ TaxiFareModel/data.py
@@ -0,0 +1,30 @@
+import pandas as pd
+
+# AWS_BUCKET_PATH = "s3://wagon-public-datasets/taxi-fare-train.csv"
+AWS_BUCKET_PATH = "https://wagon-public-datasets.s3.amazonaws.com/taxi-fare-ny/train_10k.csv"
+# AWS_BUCKET_PATH = "https://storage.cloud.google.com/wagon-data-852-ayre/data/train_1k.csv"
+
+def get_data():
+    '''returns a DataFrame with nrows from s3 bucket'''
+    df = pd.read_csv(AWS_BUCKET_PATH)
+    return df
+
+
+def clean_data(df, test=False):
+    df = df.dropna(how='any', axis='rows')
+    df = df[(df.dropoff_latitude != 0) | (df.dropoff_longitude != 0)]
+    df = df[(df.pickup_latitude != 0) | (df.pickup_longitude != 0)]
+    if "fare_amount" in list(df):
+        df = df[df.fare_amount.between(0, 4000)]
+    df = df[df.passenger_count < 8]
+    df = df[df.passenger_count >= 0]
+    df = df[df["pickup_latitude"].between(left=40, right=42)]
+    df = df[df["pickup_longitude"].between(left=-74.3, right=-72.9)]
+    df = df[df["dropoff_latitude"].between(left=40, right=42)]
+    df = df[df["dropoff_longitude"].between(left=-74, right=-72.9)]
+    return df
+
+
+if __name__ == '__main__':
+    df = get_data()
+    # print(df)
